d1bffef69b85158155ce1b44ff7d74c49462d469
solean <he15man2007@gmail.com>
2014-07-30 21:30:33 +0200

    list items are now dynamic

--- js/router.js
+++ js/router.js
@@ -1,3 +1,9 @@
 Checkmate.Router.map(function() {
 	this.resource('checkmate', { path: '/' });
 });
+
+Checkmate.CheckmateRoute = Ember.Route.extend({
+	model: function() {
+		return this.store.find('todo');
+	}
+});
