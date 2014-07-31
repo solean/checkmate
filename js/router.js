f83ab02d01e045cd5ef5494b6c2099feb330dc0a
solean <he15man2007@gmail.com>
2014-07-31 14:19:28 +0200

    separated list items as a child route

--- js/router.js
+++ js/router.js
@@ -1,9 +1,17 @@
 Checkmate.Router.map(function() {
-	this.resource('checkmate', { path: '/' });
+	this.resource('checkmate', { path: '/' }, function() {
+		//additional child routes will go here later
+	});
 });
 
 Checkmate.CheckmateRoute = Ember.Route.extend({
 	model: function() {
 		return this.store.find('todo');
+	}
+});
+
+Checkmate.CheckmateIndexRoute = Ember.Route.extend({
+	model: function () {
+		return this.modelFor('checkmate');
 	}
 });
